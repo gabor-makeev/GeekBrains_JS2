6c3c6aea5cc5fd525d05a5e9abd5cea387909a11
gabormakeev <gabor.makejev@gmail.com>
2021-05-20 03:39:29 +0300

    Урок 3

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
+// Решение 1-го задания
+const makeGETRequest = (url) => {
+  return new Promise((resolve, reject) => {
+    let xhr;
+
+    if (window.XMLHttpRequest)
+      xhr = new XMLHttpRequest();
+    else if (window.ActiveXObject)
+      xhr = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(xhr.status);
+        }
+      }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send();
+  });
+}
+
 class GoodsItem {
-  constructor(title = 'unknown', price = 0, currency = '$', imageSrc = 'images/empty-image.svg', imageClass = 'goods-item-empty') {
-    this.title = title;
+  constructor(product_name = 'unknown', price = 0, currency = '$', imageSrc = 'images/empty-image.svg', imageClass = 'goods-item-empty') {
+    this.product_name = product_name;
     this.price = price;
     this.currency = currency;
     this.imageSrc = imageSrc;
@@ -10,9 +35,9 @@ class GoodsItem {
   render() {
     return `<div class="goods-item">
                 <img src="${this.imageSrc}" class="${this.imageClass}" alt="product image">
-                <h3>${this.title}</h3>
+                <h3>${this.product_name}</h3>
                 <p>${this.currency}${this.price}</p>
-                <button>Купить</button>
+                <button data-good-id="${this.product_name}">Купить</button>
             </div>`;
   }
 }
@@ -20,18 +45,34 @@ class GoodsItem {
 class GoodsList {
   constructor() {
     this.goods = [];
+    this.API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
   }
 
+  // Решение 3-го задания. Часть 1
   fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50  },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
+    return new Promise(resolve => {
+      makeGETRequest(`${this.API_URL}/catalogData.json`)
+          .then((goods) => {
+            this.goods = JSON.parse(goods);
+            resolve();
+          }).catch((error) => {
+            console.log(`Error: ${error}`);
+      })
+    })
   }
 
-  // Решение 2-го задания // параметр quantity добавил на случай если в будущем появится информация о количестве товаров в наличии
+  // Проверка нажатия на кнопку покупки в каталоге
+  purchaseHandler(event, cart) {
+    if (event.target.tagName === 'BUTTON') {
+      this.goods.forEach(good => {
+        if (good.product_name === event.target.dataset.goodId) {
+          cart.addItemToCart(good);
+          cart.render();
+        }
+      });
+    }
+  }
+
   calculateTotalPrice(quantity = 1) {
     return this.goods.reduce((sum, good) => {
       return sum + (good.price * quantity);
@@ -41,58 +82,48 @@ class GoodsList {
   render() {
     let html = '';
     this.goods.forEach(good => {
-      let goodItem = new GoodsItem(good.title, good.price);
+      let goodItem = new GoodsItem(good.product_name, good.price);
       html += goodItem.render();
     });
     document.querySelector('.goods-list').innerHTML = html;
-    // на время этого PR-а вставил вывод решения 2-го задания в заголовок
-    document.querySelector('.header-logo').textContent += `Тут вы можете купить товаров на $${this.calculateTotalPrice()}`;
   }
 }
 
-// Решение 1-го задания
 class CartItem extends GoodsItem {
-  // функция добавляет свойство количества
-  setQuantity(quantity = 1) {
+  constructor(product_name, price, quantity = 1, currency, imageSrc, imageClass) {
+    super(product_name, price, currency, imageSrc, imageClass);
     this.quantity = quantity;
   }
 
-  // функция для подсчета общей стоимости товара с учетом его количества
   getTotalPrice() {
     return this.quantity * this.price;
   }
 
-  // Пытался модифицировать render() родителя, но пока что не придумал как лаконично вставить/изменить его return // пришлось переписать..
-  renderCartItem() {
-    return `<div>
+  render() {
+    return `<div class="goods-item">
                 <img src="${this.imageSrc}" class="${this.imageClass}" alt="product image">
-                <h3>${this.title}</h3>
+                <h3>${this.product_name}</h3>
                 <p>Количество: ${this.quantity}</p>
                 <p>Общая стоимость: ${this.getTotalPrice()}</p>
-                <button>Удалить</button>
+                <button data-good-id="${this.product_name}">Удалить</button>
             </div>`
   }
 }
 
-// Решение 1-го задания
 class Cart extends GoodsList {
-
-  // Добавление товара в массив корзины
   pushToCart(item) {
-    this.goods.push({title: item.title, price: item.price, quantity: 1});
+    this.goods.push({product_name: item.product_name, price: item.price, quantity: 1});
   }
 
-  // Увеличение количества единиц товара
   increaseQuantity(item) {
     item.quantity++;
   }
 
-  // Проверка присутствия товара в корзине...
   isInCart(unknownItem, cartItem) {
-    return unknownItem.title === cartItem.title;
+    return unknownItem.product_name === cartItem.product_name;
   }
 
-  // Главная функция для добавления товара в корзину... надеюсь не очень запутанно вышло
+  // Решение 2-го задания
   addItemToCart(item) {
     let inCart = false;
     if (this.goods.length) {
@@ -111,19 +142,62 @@ class Cart extends GoodsList {
     }
   }
 
-  // Удаление из корзины либо уменьшение количества единиц товара
+  // Проверка нажатия на кнопку удаления из корзины
+  removeHandler(event) {
+    if (event.target.tagName === 'BUTTON') {
+      this.goods.forEach(good => {
+        if (event.target.dataset.goodId === good.product_name) {
+          this.removeFromCart(good);
+        }
+      });
+    }
+  }
+
+  // Решение 2-го задания
   removeFromCart(item) {
     for (let idx = 0; idx < this.goods.length; idx++) {
-      if (this.goods[idx].title === item.title) {
+      if (this.goods[idx].product_name === item.product_name) {
         if (this.goods[idx].quantity > 1)
-          this.goods.quantity--;
+          this.goods[idx].quantity--;
         else
           this.goods.splice(idx, 1);
       }
     }
+    this.render();
+  }
+
+  // Решение 2-го задания // Не уверен до конца, но мне показалось более логичным переписать метод рендер для корзины из-за необходимости создать CartItem и обращения к блоку .cart-list
+  render() {
+    let html = ``;
+    this.goods.forEach(good => {
+      let cartItem = new CartItem(good.product_name, good.price, good.quantity);
+      html += cartItem.render();
+    });
+    document.querySelector('.cart-list').innerHTML = html;
   }
 }
 
 const catalog = new GoodsList();
-catalog.fetchGoods();
-catalog.render();
+const cart = new Cart();
+
+// Решение 3-го задания. Часть 2
+catalog.fetchGoods()
+    .then(() => {
+      catalog.render();
+    });
+
+cart.render();
+
+document.querySelector('.goods-list')
+    .insertAdjacentHTML('beforebegin', `<h2>Каталог</h2>`);
+document.querySelector('.cart-list')
+    .insertAdjacentHTML('beforebegin',  `<h2>Корзина</h2>`);
+
+document.querySelector('.goods-list')
+    .addEventListener('click', event => {
+      catalog.purchaseHandler(event, cart);
+    });
+document.querySelector('.cart-list')
+    .addEventListener('click', event => {
+      cart.removeHandler(event);
+    });
